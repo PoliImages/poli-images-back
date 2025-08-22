2091090b2cfa12b39f130db9e370d05b3d65c74a
Carolina <carolinalansoni@gmail.com>
2025-08-22 11:36:38 -0300

    pacotes iniciais

---
+++ poli_images_back/bin/poli_images_back.dart
@@ -0,0 +1,5 @@
+import 'package:poli_images_back/poli_images_back.dart' as poli_images_back;
+
+void main(List<String> arguments) {
+  print('Hello world: ${poli_images_back.calculate()}!');
+}
---
+++ poli_images_back/test/poli_images_back_test.dart
@@ -0,0 +1,8 @@
+import 'package:poli_images_back/poli_images_back.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
