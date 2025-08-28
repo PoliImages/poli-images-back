44fddab5689cd40ca84101355024375805147049
Carolina <carolinalansoni@gmail.com>
2025-08-28 12:15:47 -0300

    página cadastro funcionando

--- bin/poli_images_back.dart
+++ bin/poli_images_back.dart
@@ -5,44 +5,50 @@ import 'package:shelf/shelf_io.dart' as shelf_io;
 import 'package:shelf_router/shelf_router.dart';
 import 'package:dotenv/dotenv.dart';
 import 'package:mongo_dart/mongo_dart.dart';
-import 'package:poli_images_back/auth_service.dart'; // Importa nosso novo serviço
+import 'package:poli_images_back/auth_service.dart';
+// 1. Importa o novo pacote de CORS
+import 'package:shelf_cors_headers/shelf_cors_headers.dart';
 
 void main() async {
   final dotEnv = DotEnv(includePlatformEnvironment: true)..load();
   final mongoUri = dotEnv['MONGO_URI'];
-  print(mongoUri);
+
   if (mongoUri == null) {
     print('‼️ Erro: Variável de ambiente MONGO_URI não encontrada.');
     return;
   }
 
-  // Conecta ao banco de dados UMA VEZ quando o servidor inicia
   final db = await Db.create(mongoUri);
   await db.open();
   print('✅ Conectado ao MongoDB Atlas!');
 
-  // Cria uma instância do nosso serviço, passando a conexão com o banco
   final authService = AuthService(db);
-  
   final app = Router();
 
   app.get('/', (Request request) {
     return Response.ok('API do Poli Images está funcionando!');
   });
 
-  // --- ROTA DE CADASTRO ATUALIZADA ---
   app.post('/api/auth/register', (Request request) async {
     final requestBody = await request.readAsString();
     final result = await authService.registerUser(requestBody);
 
     return Response(
-      result['status'], // Código de status (201, 400, 409, 500)
+      result['status'],
       body: jsonEncode({'message': result['message']}),
       headers: {'Content-Type': 'application/json'},
     );
   });
 
-  final handler = const Pipeline().addHandler(app);
+  // 2. Cria o "porteiro" (middleware) de CORS
+  // O overrideHeaders com '*' permite requisições de qualquer origem.
+  final corsMiddleware = corsHeaders();
+
+  // 3. Atualiza o handler para usar o Pipeline com o CORS
+  final handler = const Pipeline()
+      .addMiddleware(corsMiddleware) // Adiciona o porteiro do CORS
+      .addHandler(app);
+
   final server = await shelf_io.serve(handler, InternetAddress.anyIPv4, 8080);
 
   print('🚀 Servidor rodando em http://localhost:${server.port}');
