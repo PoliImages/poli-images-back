eec5b3152dc92cae0a57d7b564564f1348b4b3b4
IsabellaPassarelli <isabellapassarelli025@gmail.com>
2025-10-25 12:14:18 -0300

    atualiza testes

--- .dart_tool/build/entrypoint/build.dart
+++ .dart_tool/build/entrypoint/build.dart
@@ -1,14 +1,11 @@
-// @dart=3.6
 // ignore_for_file: directives_ordering
-// build_runner >=2.4.16
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:build_runner_core/build_runner_core.dart' as _i1;
 import 'package:source_gen/builder.dart' as _i2;
 import 'package:mockito/src/builder.dart' as _i3;
 import 'package:build_config/build_config.dart' as _i4;
-import 'dart:isolate' as _i5;
-import 'package:build_runner/src/build_script_generate/build_process_state.dart'
-    as _i6;
+import 'package:build_resolvers/builder.dart' as _i5;
+import 'dart:isolate' as _i6;
 import 'package:build_runner/build_runner.dart' as _i7;
 import 'dart:io' as _i8;
 
@@ -27,6 +24,18 @@ final _builders = <_i1.BuilderApplication>[
     hideOutput: false,
     defaultGenerateFor: const _i4.InputSet(include: [r'test/**']),
   ),
+  _i1.apply(
+    r'build_resolvers:transitive_digests',
+    [_i5.transitiveDigestsBuilder],
+    _i1.toAllPackages(),
+    isOptional: true,
+    hideOutput: true,
+    appliesBuilders: const [r'build_resolvers:transitive_digest_cleanup'],
+  ),
+  _i1.applyPostProcess(
+    r'build_resolvers:transitive_digest_cleanup',
+    _i5.transitiveDigestCleanup,
+  ),
   _i1.applyPostProcess(
     r'source_gen:part_cleanup',
     _i2.partCleanup,
@@ -34,13 +43,12 @@ final _builders = <_i1.BuilderApplication>[
 ];
 void main(
   List<String> args, [
-  _i5.SendPort? sendPort,
+  _i6.SendPort? sendPort,
 ]) async {
-  await _i6.buildProcessState.receive(sendPort);
-  _i6.buildProcessState.isolateExitCode = await _i7.run(
+  var result = await _i7.run(
     args,
     _builders,
   );
-  _i8.exitCode = _i6.buildProcessState.isolateExitCode!;
-  await _i6.buildProcessState.send(sendPort);
+  sendPort?.send(result);
+  _i8.exitCode = result;
 }
